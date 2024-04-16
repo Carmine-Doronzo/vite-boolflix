cebd7e1f9da325dcc295d6071387fbc99d51071d
Carmine-Doronzo <carmine.doronzo92@gmail.com>
2024-04-16 16:29:11 +0200

    print in app vue movies and tvSeries

--- src/App.vue
+++ src/App.vue
@@ -7,15 +7,32 @@ export default {
   },
   data(){
     return{
-      movie:db.film
+      movies:db.film,
+      tvSeries:db.serieTv
     }
   }
 }
-
+//title,originalTitle,language,vote
 </script>
 
 <template>
   <HeaderComponent/>
+  <div>
+    <h1>FILM</h1>
+    <ul v-for="movie in movies">
+      <li>{{ movie.title }}</li>
+      <li>{{ movie.originalTitle }}</li>
+      <li>{{ movie.language }}</li>
+      <li>{{ movie.vote }}</li>
+    </ul>
+    <h1>SERIE TV</h1>
+    <ul v-for="serie in tvSeries">
+      <li>{{ serie.title }}</li>
+      <li>{{ serie.originalTitle }}</li>
+      <li>{{ serie.language }}</li>
+      <li>{{ serie.vote }}</li>
+    </ul>
+  </div>
 </template>
 
 <style >
